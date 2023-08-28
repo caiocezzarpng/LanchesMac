562aede77ecd8e70556c4e926d8d8094916d0fcc
Caio Cesar Sousa Bandeira <caio.bandeira@fitbank.com.br>
2023-08-28 15:39:45 -0300

    feat: aplicando os atributos Data Annotations

--- LanchesMac/Models/Lanche.cs
+++ LanchesMac/Models/Lanche.cs
@@ -1,15 +1,47 @@
-﻿namespace LanchesMac.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesMac.Models
 {
+    [Table("Lanches")]
     public class Lanche
     {
+        [Key]
         public int LancheId { get; set; }
+
+        [Required(ErrorMessage ="O nome do lanche deve ser informado")]
+        [Display(Name ="Nome do Lanche")]
+        [StringLength(80, MinimumLength = 10, ErrorMessage ="O {0} deve ter no mínimo {1} e no máximo {2} caracteres")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "a descrição do lanche deve ser informada")]
+        [Display(Name = "Descrição do lanche")]
+        [StringLength(200, MinimumLength = 20, ErrorMessage = "O {0} deve ter no mínimo {1} e no máximo {2} caracteres")]
         public string DescricaoCurta { get; set; }
+
+        [Required(ErrorMessage = "a descrição detalhada do lanche deve ser informada")]
+        [Display(Name = "Descrição detalhada do lanche")]
+        [StringLength(200, MinimumLength = 20, ErrorMessage = "O {0} deve ter no mínimo {1} e no máximo {2} caracteres")]
         public string DescricaoDetalhada { get; set; }
+
+        [Required(ErrorMessage = "Informe o preço do lanche")]
+        [Display(Name = "Preço")]
+        [Column(TypeName ="decimal(10,2)")]
+        [Range(1,999.99, ErrorMessage = "O preço deve estar entre 1 e 999,99")]
         public decimal Preco { get; set; }
+
+        [Display(Name ="Caminho imagem normal")]
+        [StringLength(200, ErrorMessage = "O {0} deve ter no máximo {1} caracteres")]
         public string ImagemUrl { get; set; }
+
+        [Display(Name = "Caminho imagem miniatura")]
+        [StringLength(200, ErrorMessage = "O {0} deve ter no máximo {1} caracteres")]
         public string ImagemThumbnailUrl { get; set; }
+
+        [Display(Name = "Preferido?")]
         public bool IsLanchePreferido { get; set; }
+
+        [Display(Name = "Estoque")]
         public bool EmEstoque { get; set; }
 
         public int CategoriaId { get; set; }
